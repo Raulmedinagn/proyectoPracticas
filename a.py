1a3646ee1ba92fbb031ccbf1107c5f5a26fea6a4
raul <raulmedinagn@gmail.com>
2020-11-04 15:03:43 +0100

    Base de datos, remodelación del front

--- a.py
+++ a.py
@@ -1,18 +1,20 @@
-from flask import Flask ,redirect, url_for, render_template, request
+from flask import Flask ,redirect, url_for, render_template, request, jsonify
+from tinydb import TinyDB, Query
+
+from pruebanltk import Analizar
 app = Flask(__name__)
 
-#restflask
-from flask_restful import Api
-from restful.prueba_api import Helloword
-from pruebanltk import Analizar
 
-api = Api(app)
-api.add_resource(Helloword, '/api')
+db = TinyDB('static/db.json')
 
+def insert(nombre,edad,email):
+	db.insert({'nombre': nombre, 'edad': edad, 'email': email})
 
+print(db.all())
 
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET','POST'])
 def home():
 	if request.method == "POST":
 		frase = request.form["fr"]
@@ -27,6 +29,24 @@ def analizar(frase_recogida):
 	palabras_importantes = " ".join(lista)
 	return palabras_importantes
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	if request.method == "POST":
+		nombre = request.form["nombre"]
+		edad = request.form["edad"]
+		email = request.form["email"]
+		print(nombre)
+		insert(nombre, edad, email)
+		return redirect(url_for("bbdd"))
+	else:
+		return render_template('login.html')
+
+@app.route('/bbdd')
+def bbdd():
+	items = []
+	for item in db:
+		items.append(item)
+	return jsonify(items)
 
 
 if __name__ == "__main__":
--- restful/prueba_api.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-
-
-class Helloword(Resource):
-    def get(self):
-        return {'hola': 'mundo'}
